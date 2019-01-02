d60508332b5cae94f2bc2ebeba91048670cc4284
Zac Jones <zacjones91@gmail.com>
2019-01-02 09:52:56 -0600

    testing deploy fix

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var autoprefixer = require('gulp-autoprefixer');
 var pkg = require('./package.json');
 var browserSync = require('browser-sync').create();
 
+// testing different deploy location
+
 // Set the banner content
 var banner = ['/*!\n',
   ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
